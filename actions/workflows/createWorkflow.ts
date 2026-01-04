d2ab9a060409afbc0bdb49478cef34449f12ebf4
Maxy-Chu <mingxi_zhu@berkeley.edu>
2026-01-04 14:44:58 -0800

    create workflow, Zod, and tanstack query for database mutation

---
+++ actions/workflows/createWorkflow.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { createWorkflowSchema, createWorkflowSchemaType } from "@/schema/workflows";
+import { WorkflowStatus } from "@/types/workflow";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export async function CreateWorkflow(form: createWorkflowSchemaType) {
+    // need Zod validation here again because
+    // a server action is a HTTP endpoint exposed to the internet
+    // and we cannot fully trust the client
+    // even though we already validated on the client side
+    const { success, data } = createWorkflowSchema.safeParse(form);
+    if (!success) {
+        throw new Error("Invalid form data");
+    }
+
+    const { userId } = auth();
+    if (!userId) {
+        throw new Error("Not authenticated");
+    }
+
+    const result = await prisma.workflow.create({
+        data: {
+            userId,
+            status: WorkflowStatus.DRAFT,
+            definition: "TODO",
+            ...data,
+        },
+    });
+    if (!result) {
+        throw new Error("Failed to create workflow");
+    }
+    redirect(`/workflows/editor/${result.id}`);
+}
---
+++ app/(dashboard)/workflows/_components/CreateWorkflowDialog.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import React, { useCallback, useState } from 'react'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Layers2Icon } from 'lucide-react';
+import CustomDialogHeader from '@/components/CustomDialogHeader';
+import { createWorkflowSchema, createWorkflowSchemaType } from '@/schema/workflows';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { useMutation } from '@tanstack/react-query';
+import { CreateWorkflow } from '@/actions/workflows/createWorkflow';
+import { toast } from 'sonner';
+
+function CreateWorkflowDialog({ triggerText }: { triggerText?: string }) {
+  const [open, setOpen] = useState(false);
+
+  // z.infer is used to infer the user input type from the schema
+  const form = useForm<createWorkflowSchemaType>({
+    resolver: zodResolver(createWorkflowSchema),
+    defaultValues: {},
+  });
+
+  // Mutation for form submission to database
+  const { mutate, isPending } = useMutation({
+    mutationFn: CreateWorkflow,
+    onSuccess: () => {
+      toast.success("Workflow created", { id: "create-workflow" });
+    },
+    onError: () => {
+      toast.error("Failed to create workflow", { id: "create-workflow" });
+    },
+  })
+
+  // Handle form submission
+  const onSubmit = useCallback(
+    (values: createWorkflowSchemaType) => {
+      toast.loading("Creating workflow...", { id: "create-workflow" });
+      mutate(values);
+    },
+    [mutate]
+  );
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button>{triggerText ?? "Create workflow"}</Button>
+      </DialogTrigger>
+      <DialogContent className='px-0'>
+        <CustomDialogHeader
+          icon={Layers2Icon}
+          title="Create Workflow"
+          subTitle="Start building your workflow"
+        />
+        <div className='p-6'>
+          <Form {...form}>
+            <form className='space-y-8 w-full' onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='flex gap-1 items-center'>
+                      Name
+                      <p className='text-xs text-primary'>(required)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Choose a descriptive and unique name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='flex gap-1 items-center'>
+                      Description
+                      <p className='text-xs text-muted-foreground'>(optional)</p>
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea className='resize-none' {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      Provide a brief description for your workflow to help memorize it.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className='w-full' disabled={isPending}>Proceed</Button>
+            </form>
+          </Form>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default CreateWorkflowDialog
